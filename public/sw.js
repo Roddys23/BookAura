2a8b937b97bb5725bc5fd02be6f145ecb89bdba3
Roddys23 <143403000+Roddys23@users.noreply.github.com>
2026-03-08 22:58:14 +0000

    local files

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 /* global workbox */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js");
 
-const APP_VERSION = "1773010474589";
+const APP_VERSION = "1773010657468";
 const CACHE_PREFIXES = ["aura-pages-", "aura-audio-", "aura-images-", "aura-api-", "aura-flight-mode-"];
 const ACTIVE_CACHES = new Set([
   `aura-pages-${APP_VERSION}`,
